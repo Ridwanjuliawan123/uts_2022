05599417afc23e02501cc049805b71fad7f14da5
Ridwanjuliawan123 <113905013+Ridwanjuliawan123@users.noreply.github.com>
2022-09-19 19:33:09 +0700

    Add files via upload

---
+++ ridwan_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "nama: guntur"<<endl;
+    
+    cout << "kelas: xrpl2"<<endl;
+   
+   cout << "nilai: 85" <<endl;
+ }
